66162c77390a1a698e1a14baf2d7aaebc65692b8
Guillermo Linares <willy_trooper@hotmail.com>
2021-05-04 19:34:32 -0500

    actualizacion de https

--- src/Helpers/getGifs.js
+++ src/Helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=Rr2CoVmweoYG9cVdhhH6Ybn6p9IySvyv&q=${encodeURI(category)}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Rr2CoVmweoYG9cVdhhH6Ybn6p9IySvyv&q=${encodeURI(category)}&limit=10`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
